7b6d3cd812862dd0ea3beeddbf13d4de3abb0824
Christina <christina@ChristinasMacAir.lan>
2015-10-28 20:19:18 -0400

    Added bootstrap and socialization gems and fixed bug so users can signup or login

--- facebook_clone/app/controllers/application_controller.rb
+++ facebook_clone/app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :find_users
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation)}
+  end
+
+  private
+
+  def not_authenticated
+  	redirect_to login_path
+  end
+
+  def find_users
+  	@users = User.all
+  end
+
 end
