256ce124f1befc1904b75d20983817b7620bfab3
Christina <christina@ChristinasMacAir.lan>
2015-10-28 20:20:12 -0400

    Created scaffold to give users the ability to create posts

--- facebook_clone/db/schema.rb
+++ facebook_clone/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028235843) do
+ActiveRecord::Schema.define(version: 20151029001940) do
 
   create_table "follows", force: :cascade do |t|
     t.string   "follower_type"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20151028235843) do
 
   add_index "mentions", ["mentionable_id", "mentionable_type"], name: "fk_mentionables"
   add_index "mentions", ["mentioner_id", "mentioner_type"], name: "fk_mentions"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
